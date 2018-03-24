682bd7cd206f60c3369c457ef35c7695ceb618c8
Sonal <cs0078087@gmail.com>
2018-03-24 14:28:44 +0530

    my firs commit

---
+++ src/main/java/loginDemo/LoginDemo.java
@@ -0,0 +1,9 @@
+package loginDemo;
+
+public class LoginDemo {
+
+	public void loginDemo(){
+		System.out.println("my login gitDemo...");
+	}
+
+}
---
+++ src/test/java/loginDemoTest/TestCaseLogin.java
@@ -0,0 +1,12 @@
+package loginDemoTest;
+
+
+
+public class TestCaseLogin {
+
+	public void testLogin(){
+		System.out.println("Login Test in gitDemo..");
+	}
+	
+	
+}
