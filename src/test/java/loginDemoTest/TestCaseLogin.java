bb401f4415e30d6c35e54ef89d693bff100b8108
Sonal <cs0078087@gmail.com>
2018-03-24 15:48:56 +0530

    my firs commit

--- src/test/java/loginDemoTest/TestCaseLogin.java
+++ src/test/java/loginDemoTest/TestCaseLogin.java
@@ -6,6 +6,8 @@ public class TestCaseLogin {
 
 	public void testLogin(){
 		System.out.println("Login Test in gitDemo..");
+		System.out.println("A new change in my project");
+		System.out.println("Change for pull functionality..");
 	}
 	
 	
